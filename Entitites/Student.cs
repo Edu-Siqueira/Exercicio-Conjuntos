cb9d77757522d734b46ea45e8c7dadeeaf62ef5f
Eduardo-SSiqueira <edusiqueira150@gmail.com>
2021-08-04 22:16:48 -0300

    Projeto adicionado.

---
+++ Entitites/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercicioConjuntos.Entitites
+{
+    class Student
+    {
+        public int Id { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(int id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ExercicioConjuntos.Entitites;
+using System;
+using System.Collections.Generic;
+
+namespace ExercicioConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> StudentList = new();
+
+            Console.Write("How many students in course A? ");
+            int countA = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= countA; i++)
+            {
+                int studentIdA = int.Parse(Console.ReadLine());
+                Student A = new Student(studentIdA);
+                StudentList.Add(A);
+            }
+
+            Console.Write("How many students in course B? ");
+            int countB = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= countB; i++)
+            {
+                int studentIdB = int.Parse(Console.ReadLine());
+                Student B = new Student(studentIdB);
+                StudentList.Add(B);
+            }
+
+            Console.Write("How many students in course C? ");
+            int countC = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= countC; i++)
+            {
+                int studentIdC = int.Parse(Console.ReadLine());
+                Student C = new Student(studentIdC);
+                StudentList.Add(C);
+            }
+
+            Console.WriteLine($"Total students: {StudentList.Count}");
+        }
+    }
+}                                   
